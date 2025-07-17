55f27424ff429abab1b6d01c43bf3a7d415b8313
Saurav Punjabi <sauravpunjabi123@gmail.com>
2025-07-17 20:02:24 +0530

    implmented navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import Navbar from './components/Navbar';
+import gsap from 'gsap';
+import { ScrollTrigger, SplitText } from 'gsap/all';
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+   <main>
+        <Navbar />
+   </main>
   )
 }
 
