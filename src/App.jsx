1a51f6761c99fdaa86e38ea2475a80e1c463a75f
Saurav Punjabi <sauravpunjabi123@gmail.com>
2025-07-19 01:39:30 +0530

    added Menu functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-import gsap from 'gsap';
-import { ScrollTrigger, SplitText } from 'gsap/all';
 import Cocktails from './components/Cocktails';
 import About from './components/About';
 import Art from './components/Art';
-gsap.registerPlugin(ScrollTrigger, SplitText);
+import Menu from './components/Menu';
+import Contact from './components/Contact';
 
 const App = () => {
   return (
@@ -15,7 +14,8 @@ const App = () => {
         <Cocktails />
         <About />
         <Art />
-        
+        <Menu />
+        <Contact />
    </main>
   )
 }
